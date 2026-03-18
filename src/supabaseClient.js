c7c480cfb31e1bf07ad13c7265af486283f54576
amiinem7 <amine.mabtoul@outlook.fr>
2026-03-18 13:21:58 +0100

    fix supabase anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://jmanxlmzvfnhpgcxsqly.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_DtXnt43MsZidSw6bRZb8qg_G06fLPh-'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptYW54bG16dmZuaHBnY3hzcWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1ODE4NDYsImV4cCI6MjA4OTE1Nzg0Nn0.O6Mjh2KRydHtCj4ZxyZaaqzcleOQOq4jC01zoSaYxws'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
